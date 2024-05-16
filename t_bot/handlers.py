74f6791f9b7ce787d3c05c5a95bed3171b4e478e
Abdulqodir <abdulkodir87@gmail.com>
2024-05-16 15:09:05 +0500

    start

--- t_bot/handlers.py
+++ t_bot/handlers.py
@@ -1,17 +1,25 @@
 from aiogram import Dispatcher, types, F
 from aiogram.filters import CommandStart
 
-from t_bot.keyboards import app_kb
+from keyboards import apple_kb
 
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
-async def start(message: types.Message):
-    await message.answer("qalesa", reply_markup=app_kb)
+async def start(msg: types.Message):
+    await msg.answer("Salom", reply_markup=apple_kb)
 
 
 @dp.message(F.func(lambda msg: msg.web_app_data.data))
 async def get_btn(msg: types.Message):
-    print(msg)
-    await msg.answer(msg.web_app_data.data)
+    text = msg.web_app_data.data
+    products = text.split("|")
+    for i in range(len(products)):
+        title = products[i].split('/')[0]
+        price = int(products[i].split('/')[1])
+        quantity = int(products[i].split('/')[2])
+        await msg.answer(text=f"Nomi: {title}\n"
+                              f"Narxi: {price}\n"
+                              f"Soni: {quantity}\n"
+                              f"Umumiy narxi: {quantity * price}$")
--- t_bot/keyboards.py
+++ t_bot/keyboards.py
@@ -2,6 +2,6 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
 web_app = WebAppInfo(url="https://abdulkodir0.github.io/modul_11.github.io/")
 
-app_kb = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="Mini app", web_app=web_app)]
+apple_kb = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Mini Up', web_app=web_app)]
 ], resize_keyboard=True)
